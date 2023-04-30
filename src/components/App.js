e17170c016f959c3800580a64538ae30e4564615
kwamebrowne <theeecryptoking@gmail.com>
2023-04-30 00:20:29 -0500

    3rd react lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,16 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+//  <About />  
 
 
 function App() {
   return (
-    <div>
+    <div id='about'>
+    <h2 id='about'>About</h2>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      {/* <About />  */}
     </div>
   );
 }
